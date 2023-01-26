babc7215715b22e44df27625382403d7c2ddbea9
Tanner Oakes <tanner.w.oakes@gmail.com>
2023-01-25 22:28:59 -0600

    add USER & QUIT

--- server.go
+++ server.go
@@ -8,8 +8,58 @@ import (
 	"strings"
 )
 
-type Server struct {
-	Users map[string]string
+type conn struct {
+	conn   net.Conn
+	path   string
+	binary bool
+}
+
+func NewConnection(newConn net.Conn) *conn {
+	return &conn{conn: newConn, path: "/", binary: true}
+}
+
+func (c *conn) printPath() {
+	fmt.Fprint(c.conn, c.path)
+}
+
+func (c *conn) closeConn() {
+	fmt.Fprint(c.conn, "221 Bye.\n")
+	err := c.conn.Close()
+	if err != nil {
+		fmt.Printf("internal server error: %s", err)
+	}
+}
+
+func (c *conn) handleConn() {
+
+	input := bufio.NewScanner(c.conn)
+	for input.Scan() {
+		// grab verb and param
+		input := input.Text()
+		splitInput := strings.SplitN(input, " ", 2)
+		var param string
+		verb := splitInput[0]
+		if len(splitInput) > 1 {
+			param = splitInput[1]
+		}
+		fmt.Printf("VERB: %s\tPARAM: %s\n", verb, param)
+		ctx := context.Background()
+		c.processCommand(ctx, verb, param)
+	}
+
+}
+
+func (c *conn) processCommand(ctx context.Context, verb, param string) {
+	switch verb {
+	case "USER":
+		fmt.Fprintf(c.conn, "230 login successful.\n")
+	case "PWD":
+		c.printPath()
+	case "QUIT":
+		c.closeConn()
+	default:
+		fmt.Println("Not a supported command")
+	}
 }
 
 func ListenAndServe() {
@@ -23,40 +73,7 @@ func ListenAndServe() {
 		if err != nil {
 			fmt.Printf("error trying to accept connection: %v", err)
 		}
-		go handleConn(conn)
-	}
-}
-
-func handleConn(conn *net.TCPConn) {
-	// switch command {
-	// case "ADD":
-	// default:
-	// 	fmt.Println("Command not found")
-	// }
-	fmt.Println("Received communication from client")
-	input := bufio.NewScanner(conn)
-	// _, err := conn.ReadFrom(conn)
-	// if err != nil {
-	// 	fmt.Printf("error reading from connection: %v", err)
-	// }
-	for input.Scan() {
-		// grab verb and param
-		input := input.Text()
-		splitInput := strings.SplitN(input, " ", 2)
-		verb, param := splitInput[0], splitInput[1]
-		fmt.Printf("VERB: %s\tPARAM: %s\n", verb, param)
-		ctx := context.Background()
-		processCommand(ctx, verb, param)
-	}
-
-}
-
-func processCommand(ctx context.Context, verb, param string) {
-	switch verb {
-	case "ADD":
-		fmt.Println("Called the ADD verb")
-	default:
-		fmt.Println("Not a supported command")
+		go NewConnection(conn).handleConn()
 	}
 }
 
